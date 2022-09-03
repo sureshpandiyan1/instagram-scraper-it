a951eab02e123641c482b38aafb7072d13891682
Suresh P <112636345+sureshpandiyan1@users.noreply.github.com>
2022-09-03 09:43:24 +0530

    Update followersid.py

--- followersid.py
+++ followersid.py
@@ -1,11 +1,11 @@
 
 # EDITABLE FILE
 
-# ONLY INTEGERS
+
 
 # EXAMPLE:
-# followers_counts = 873
-# target_insta_id = 1234567890
+# followers_counts = 100  # enter the followers counts
+# target_insta_id = '1234567890'   # enter target insta user id
 
 followers_counts = '{}'  # enter the followers counts
 target_insta_id = '{}'   # enter target insta user id
