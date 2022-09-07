22ca3a577565872e2c3887a6fce579b763bb0da0
Suresh P <112636345+sureshpandiyan1@users.noreply.github.com>
2022-09-07 09:34:06 +0530

    auth

---
+++ auth.py
@@ -0,0 +1,14 @@
+
+#EDITABLE FILE
+
+#USER AUTH
+
+# EXAMPLE
+
+# you need to enter your gmail and passwd
+# both must be str
+# user = 'xxxxxxxx@gmail.com'
+# passwd = '****************'
+
+user = '' # your username
+passwd =  '' # your password
